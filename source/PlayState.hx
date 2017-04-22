75bc6a84db33e899e38f4d98dc2a60c2c079c4f2
Josh Schonstal <josh@schonstal.org>
2017-04-22 12:58:09 -0700

    💅 Tabs to spaces

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,15 +2,12 @@ package;
 
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
-	override public function create():Void
-	{
-		super.create();
-	}
+class PlayState extends FlxState {
+  override public function create():Void {
+    super.create();
+  }
 
-	override public function update(elapsed:Float):Void
-	{
-		super.update(elapsed);
-	}
+  override public function update(elapsed:Float):Void {
+    super.update(elapsed);
+  }
 }
