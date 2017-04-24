494036da04abcca3edef0adb1e05c6e595780015
Josh Schonstal <josh@schonstal.org>
2017-04-23 20:25:08 -0700

    👹 Make motion of shooting enemy a little more interesting

--- source/ShootingEnemy.hx
+++ source/ShootingEnemy.hx
@@ -2,10 +2,15 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxObject;
 import flixel.system.FlxSound;
+import flixel.math.FlxVector;
 
 class ShootingEnemy extends Enemy {
   var shootTimer:Float = 2;
   var shootTime:Float = 2;
+
+  private var sinAmt:Float = 0;
+  private var sinOffset:Float = 0;
+  private var sinMod:Float = 0;
 
   public function new(X:Float = 0, Y:Float = 0) {
     super();
@@ -18,9 +23,19 @@ class ShootingEnemy extends Enemy {
     animation.add("shoot", [1, 2], 10, false);
     animation.finishCallback = onAnimationComplete;
     animation.play("idle");
+
+    health = 50;
+
+    setFacingFlip(FlxObject.LEFT, true, false);
+    setFacingFlip(FlxObject.RIGHT, false, false);
   }
 
   public override function update(elapsed:Float):Void {
+    sinAmt += elapsed;
+    sinOffset = Math.sin(sinAmt + sinMod) * 5;
+
+    velocity.y = sinOffset;
+
     shootTimer -= elapsed;
     if (shootTimer <= 0) {
       shoot();
