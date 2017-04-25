58b46695151b5e0915452437a5fcc66a69a6fca0
Josh Schonstal <josh@schonstal.org>
2017-04-24 17:40:02 -0700

    🖼 Add game over image

--- source/GameOverGroup.hx
+++ source/GameOverGroup.hx
@@ -14,8 +14,10 @@ class GameOverGroup extends FlxSpriteGroup {
     super();
     exists = false;
     gameOverSprite = new FlxSprite();
-    gameOverSprite.loadGraphic("assets/images/gameOver.png");
+    gameOverSprite.loadGraphic("assets/images/gameover.png");
     add(gameOverSprite);
+
+    scrollFactor.x = scrollFactor.y = 0;
   }
 
   public override function update(elapsed:Float):Void {
